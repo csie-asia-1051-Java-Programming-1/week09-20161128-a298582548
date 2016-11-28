2225a60b013009b197ede8a53b3fbd038ce75d2a
a298582548 <105021010@live.asia.edu.tw>
2016-11-28 17:22:19 +0800

    fu....

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -5,14 +5,34 @@
 	Random randnum = new Random();
 	randnum.setSeed(23323456)
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021010 邱信喆
  */
-
+import java.util.Random;
+import java.util.Scanner;
 public class ex01 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		rnd.setSeed(scn.nextInt());
+		int v2 [] = new int[6];
+	while(true){
+	    for(int j = 0 ; j<6 ; j++){
+	    	v2[j] = rnd.nextInt(42)+1;
+	    	if(j != 0)
+	    	for(int p = j ; p < 0 ;p--){
+	    		if(v2[j] != v2[p-1]){
+	    		System.out.print(v2[j] + "\t");
+	    
+	    	}
+	    if(v2.length == 6){
+	    break;
+        }
+    	}
 }
+}
+}
+}
+	
+
+
